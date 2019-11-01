578db304859c4fca1faa1fa9c168c383bffba6b6
bmulhern2 <bmulhern09@gmail.com>
2019-11-01 05:15:24 -0400

    commit

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-/* let button = document.querySelector('#submit')
-	button.addEventListener('click', calculate); */
+let button = document.querySelector('#submit')
+	button.addEventListener('click', calculate);
 
-	/* function calculate(prompt, cost, tipPercent, total) { */
-	var prompt2 = prompt("How Much Did Your Meal Cost?");
-	prompt = parseInt(prompt2);
-	var tipAmount = 0.15;
-	var total  = (prompt * tipAmount) + prompt;
-	var output = document.querySelector('.output')
+	function calculate() {
+var input = prompt("How Much Was Your Meal?");
+var input2 = input;
+var tipAmount = 1.15;
+var total = (input * tipAmount).toFixed(2);
+var output = document.querySelector('.output');
 	output.innerHTML = 'Total: $' + total;
 	output.style.marginTop = "100px";
-
-/* }; */
+};
